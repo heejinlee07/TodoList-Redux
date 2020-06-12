8826c051efe92894d6eaac10471f79719e8da611
heejinlee07 <heejinlee07@gmail.com>
2020-06-12 11:20:42 +0900

    delete

--- src/utils/movieApi.js
+++
@@ -1,6 +0,0 @@
-import axios from "axios";
-
-export const api = axios.create({
-  baseURL: "https://api.themoviedb.org/3/",
-  // params: { api_key: "9635a2a235539b3caf57c91fce352a80"},
-});
--- src/utils/movieKey.js
+++
@@ -1,1 +0,0 @@
-export const API_KEY = "9217a59fb3e4389faf2686628dab29b7";
